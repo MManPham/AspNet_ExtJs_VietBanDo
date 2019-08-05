239c9dc4c8710608510862a7f15bdd21bf4650a2
manpham <manpham146@gmail.com>
2019-08-05 16:49:35 +0700

    Start my map training

---
+++ MyMap/MyMap/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using GetStartAspNet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GetStartAspNet.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Authorize]
+        public ActionResult Index()
+        {
+            var emailId = User.Identity.Name;
+            using (MyDatabaseEntities1 dc = new MyDatabaseEntities1())
+            {
+                var accountLogin = dc.Users.Where(user => user.EmailID == emailId).FirstOrDefault();
+
+                if (accountLogin != null) return View();
+            }
+            return HttpNotFound();
+        }
+
+        [Authorize]
+        public ActionResult FindDirects()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult MyDirection()
+        {
+            ViewBag.Message = "My Direction Page.";
+
+            return View();
+        }
+    }
+}
---
+++ MyMap/MyMap/Scripts/myMap.js
@@ -0,0 +1,14 @@
+﻿var initialize = function () {
+    var mapContainer = document.getElementById("containerMap");
+    // vbd.pathImgMap = 'http://images.vietbando.com/mapimagesws/mapimageservice.ashx?Action=GetTiles';
+    var mapProp = {
+        center: new vbd.LatLng(14.102783, 109.649506),
+        zoom: 5,
+        minZoom: 2,
+
+    };
+
+    var map = new vbd.Map(mapContainer, mapProp);
+};
+
+vbd.event.addDomListener(window, 'load', initialize);
